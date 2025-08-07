30e528f876fe907fc6944cd50caf837ddfd8a2f5
Akash Gupta <Gupta.akash.aku25@gmail.com>
2025-08-07 18:21:09 +0530

    few changes in the script

--- sum.py
+++ sum.py
@@ -1,5 +1,7 @@
 a = int(input("Enter number 1 \n"))
 b = int(input("Enter number 2 \n"))
 
+print ("The First number is  :",a)
+print ("The Second number is  :",b)
 
 print("sum of num1 and num2 is  :", a+b)
