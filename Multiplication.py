5ac354011a3c30cf62d69e5354b22a0abbaa4751
Akash Gupta <Gupta.akash.aku25@gmail.com>
2025-08-07 18:25:30 +0530

    Multiplication script

---
+++ Multiplication.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number 1 \n"))
+b = int(input("Enter number 2 \n"))
+
+print ("The First number is  :",a)
+print ("The Second number is  :",b)
+
+print("Multiplication of num1 and num2 is  :", a*b)
